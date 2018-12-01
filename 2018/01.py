ebb1c9d954ddaf5ca1e0aebf2d29f2608dd146bc
Mikael Jönsson <mikael.jonsson@fortnox.se>
2018-12-01 16:38:38 +0100

    Part two, day one

--- 2018/01.py
+++ 2018/01.py
@@ -2,8 +2,17 @@
 
 import sys
 
-lines = sys.stdin.readlines()
+def find_twice(frequencies, last = 0, visited = {}):
+    for d in frequencies:
+        last = last + d
 
+        if last in visited:
+            return last
+        else:
+            visited[last] = last
+
+    return find_twice(frequencies, last, visited)
+
+lines = sys.stdin.readlines()
 frequencies = [int(line) for line in lines]
-freq = sum(d for d in frequencies)
-print(freq)
+print(find_twice(frequencies))
